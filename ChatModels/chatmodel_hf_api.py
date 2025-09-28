cef7fb48b7f53cce2bea3bb3877d1f2b32177b9c
Amarendra kumar <kr77.amar@gmail,com>
2025-09-28 11:38:41 +0530

    first commit

---
+++ ChatModels/chatmodel_hf_api.py
@@ -0,0 +1,23 @@
+# through api call
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# llm = HuggingFaceEndpoint(
+#     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+#     task="text-generation"
+# ) # You can change the model here, but make sure it is a chat model
+
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
+
+
+
+
+
+
